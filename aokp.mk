2d6d204eb872898417b4872075573804b161e17b
steadfasterX <steadfastX@boun.cr>
2017-10-31 09:52:36 +0100

    Fix typo from branch transfer

--- aokp.mk
+++ aokp.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/lge/h811/full_h811.mk)
 
-# Inherit some common AOSCP stuff.
-$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
