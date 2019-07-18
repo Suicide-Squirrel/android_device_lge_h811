dc70b08bb13562cd9393d76568e2d19c1bde7aa4
kessaras <paul@cypheros.co>
2019-07-18 13:41:30 +0300

    h811: fingerprint: Use the last fingerprint from latest official stock LG rom
    - Add AndroidProduct makefile
    
    Change-Id: I1143ad24cb673cb5336997850d2cfcda0a40630c

--- aoscp.mk
+++ aoscp.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/lge/h811/full_h811.mk)
+$(call inherit-product, device/lge/h811/h811.mk)
 
 # Inherit some common AOSCP stuff.
 $(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
@@ -6,15 +6,9 @@ $(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := aoscp_h811
-PRODUCT_DEVICE := h811
-PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-H811
-PRODUCT_MANUFACTURER := LGE
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
-    PRODUCT_NAME="g4__tmo_us" \
-    PRIVATE_BUILD_DESC="p1__tmo_us-user 8.0 NRD90U 1717709471d0f release-keys"
+    PRODUCT_NAME="p1_tmo_us" \
+    PRIVATE_BUILD_DESC="p1_tmo_us-user 6.0 MRA58K 1731216298e5b release-keys"
 
-BUILD_FINGERPRINT := "lge/p1__tmo_us/p1:8.1.0/OPM4.171019.016.A1/4720811:user/release-keys"
+BUILD_FINGERPRINT := "lge/p1_tmo_us/p1:6.0/MRA58K/1731216298e5b:user/release-keys"
