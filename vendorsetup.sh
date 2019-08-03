9a7762e78676f04d62f8801bccec0be82eafb7f5
kessaras <paul@cypheros.co>
2019-08-03 10:23:39 +0300

    initial bringup pa

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,8 @@
-add_lunch_combo aoscp_h811-userdebug
-add_lunch_combo aoscp_h811-user
-add_lunch_combo aoscp_h811-eng
+export LLVM_ENABLE_THREADS=1
+export ANDROID_SET_JAVA_HOME=true
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export USE_NINJA=true
+
+add_lunch_combo pa_h811-userdebug
+add_lunch_combo pa_h811-user
+add_lunch_combo pa_h811-eng
